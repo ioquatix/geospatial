ce4a1a1aac680fee4ec5c8d0e435b70e643eff5b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-11-07 21:50:43 +1300

    Fix histogram indexing.

--- lib/geospatial/histogram.rb
+++ lib/geospatial/histogram.rb
@@ -7,8 +7,13 @@
 module Geospatial
 	# This location is specifically relating to a WGS84 coordinate on Earth.
 	class Histogram
-		def initialize(bins, offset = 0, scale = 1)
-			@bins = bins
+		def initialize(min = 0, max = 1, scale = 0.1)
+			@min = min
+			@max = max
+			@scale = scale
+			
+			@count = ((@max - @min) / @scale).ceil
+			@bins = [0] * @count
 			@offset = 0
 			@scale = scale
 		end
@@ -19,7 +24,7 @@ module Geospatial
 		attr :scale
 		
 		def add(value, amount = 1)
-			index = Integer((value - @offset) / @scale)
+			index = ((value - @min) / @scale).floor
 			
 			if @bins[index]
 				@bins[index] += amount
@@ -33,15 +38,31 @@ module Geospatial
 		def peaks
 			@bins.each_with_index
 		end
+		
+		def offset(index)
+			@min + (index * @scale)
+		end
+		
+		def inspect
+			buffer = String.new("\#<#{self.class}")
+			
+			@bins.each_with_index do |bin, index|
+				buffer << "\n#{offset(index).to_s.rjust(8)}: #{bin}"
+			end
+			
+			buffer << "\n>"
+		end
 	end
 	
 	class RadialHistogram < Histogram
-		def initialize(center, offset = 0, scale = 360.0)
-			super([0] * 360, offset, scale)
+		def initialize(center, min = -180, max = 180, scale = 10)
+			super(min, max, scale)
+			
+			@center = center
 		end
 		
-		def add(point)
-			
+		def add(point, value = 1)
+			super(point.bearing_from(@center), value)
 		end
 	end
 		
