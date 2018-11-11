1be121d3c1c2a17cad4a21ecc3d4f2d3d7d9ab6f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-11-11 18:04:02 +1300

    Add segments computation to histogram peaks.

--- lib/geospatial/histogram.rb
+++ lib/geospatial/histogram.rb
@@ -7,50 +7,61 @@
 module Geospatial
 	# This location is specifically relating to a WGS84 coordinate on Earth.
 	class Histogram
-		def initialize(min = 0, max = 1, scale = 0.1)
+		def initialize(min: 0, max: 1, scale: 0.1)
 			@min = min
 			@max = max
 			@scale = scale
 			
-			@count = ((@max - @min) / @scale).ceil
-			@bins = [0] * @count
+			@count = 0
+			
+			@size = ((@max - @min) / @scale).ceil
+			@bins = [0] * @size
 			@offset = 0
 			@scale = scale
 		end
 		
-		attr :bins
+		attr_accessor :bins
+		
+		attr :count
 		
 		attr :offset
 		attr :scale
+		
+		def [] index
+			@bins[index]
+		end
+		
+		def size
+			@bins.size
+		end
 		
 		def map(value)
 			((value - @min) / @scale)
 		end
 		
 		def unmap(index)
-			@min + ((index + 0.5) * @scale)
+			@min + (index * @scale)
 		end
 		
 		def add(value, amount = 1)
-			index = map(value).floor
+			index = map(value).floor % @size
 			
-			if @bins[index]
+			if !block_given? or yield(index, value)
+				@count += 1
 				@bins[index] += amount
-			else
-				@bins[index] = amount
 			end
 			
-			return self
+			return index
 		end
 		
 		def inspect
 			buffer = String.new("\#<#{self.class}")
 			
 			@bins.each_with_index do |bin, index|
-				buffer << "\n#{unmap(index).to_s.rjust(8)}: #{bin}"
+				buffer << " #{unmap(index)}: #{bin}"
 			end
 			
-			buffer << "\n>"
+			buffer << ">"
 		end
 		
 		def each
@@ -60,17 +71,75 @@ module Geospatial
 				yield unmap(index), value
 			end
 		end
+		
+		def peaks
+			Peaks.new(self)
+		end
 	end
 	
 	class RadialHistogram < Histogram
-		def initialize(center, min = -180, max = 180, scale = 10)
-			super(min, max, scale)
+		def initialize(center, min: -180, max: 180, scale: 1)
+			super(min: min, max: max, scale: scale)
 			
 			@center = center
 		end
 		
 		def add(point, value = 1)
 			super(point.bearing_from(@center), value)
+		end
+	end
+	
+	class Peaks
+		include Enumerable
+		
+		def initialize(values)
+			@values = values
+			@derivative = []
+			
+			s = @values.size
+			
+			@values.size.times do |i|
+				# Apply the Laplacian of Gaussians to compute the gradient changes:
+				# @derivative << (@values[i-2] * -1) + (@values[i-1] * -1) + (@values[i] * 4) + (@values[i+1-s] * -1) + (@values[i+2-s] * -1)
+				@derivative << (2.0 * @values[i-1]) + (-2.0 * @values[i+1-s])
+			end
+		end
+		
+		attr :derivative
+		
+		def each
+			return to_enum unless block_given?
+			
+			@derivative.each_with_index do |y2, x2|
+				x1 = x2 - 1
+				y1 = @derivative[x1]
+				
+				if (y1 <= 0 and y2 > 0) or (y1 > 0 and y2 <= 0)
+					# There has been a zero crossing, so we have a peak somewhere here:
+					g = (y2.to_f - y1.to_f)
+					m = (-y1.to_f / g)
+					
+					yield x1 + m, g
+				end
+			end
+		end
+		
+		def segments
+			return to_enum(:segments) unless block_given?
+			
+			gradients = self.to_a
+			
+			return if gradients.empty?
+			
+			index, gradient = gradients.first
+			
+			if gradient < 0
+				gradients.push gradients.shift
+			end
+			
+			gradients.each_slice(2) do |up, down|
+				yield up, down
+			end
 		end
 	end
 end
